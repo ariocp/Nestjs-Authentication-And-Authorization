ad37fda3a9494e1d254cba6efe744534c0542aaa
ariocp <ariocp@rambler.ru>
2023-10-29 12:13:26 +0300

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   src/users/users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,28 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ApiTags } from "@nestjs/swagger";
 import { JwtAuthGuard } from "../auth/guards/jwt.guard";
 import { UserId } from "../decorators/user-id.decorator";
+import { Repository } from "typeorm";
+import { UserEntity } from "./entities/user.entity";
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Controller('users')
 @ApiTags('users')
 export class UsersController {
   constructor(
-      private readonly usersService: UsersService
+      private readonly usersService: UsersService,
+      @InjectRepository(UserEntity) private repository: Repository<UserEntity>,
   ) {}
 
   @Get('me')
   @UseGuards(JwtAuthGuard)
   getMe(@UserId() id: number) {
     return this.usersService.findById(id);
+  }
+
+  @Post('delete')
+  deleteAll(): Promise<void> {
+    return this.repository.clear();
   }
 }
